a463b48b561ee9878f25c81ef0f4d75a4038ca74
JHMR18 <opulenciajhomer@gmail.com>
2024-06-01 17:00:57 +0800

    login UI

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="login-form">
+<div class="container">
+    <div class="login-form">
     <v-icon icon="mdi-account" />
     <h2>Login</h2>
     <form @submit.prevent="login">
@@ -11,10 +12,11 @@
         <label for="password">Password</label>
         <input type="password" id="password" v-model="password" required>
       </div>
-      <p>Login as <span style="color: blue; cursor: pointer; margin-bottom: 10px;">Admin</span></p>
-      <button type="submit">Login</button>
+      <p style="margin-bottom: 10px;">Login as <span style="color: navajowhite; cursor: pointer;">Admin</span></p>
+      <button type="submit" to="/dashboard">Login</button>
     </form>
   </div>
+</div>
 </template>
 
 <script>
@@ -56,16 +58,24 @@ export default {
 </script>
 
 <style scoped>
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
 .login-form {
+    background-color: rgb(33, 33, 52);;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  max-width: 300px;
+  max-width: 400px;
   margin: 0 auto;
   padding: 2rem;
   border: 1px solid #ccc;
   border-radius: 4px;
+  color: white;
 }
 
 .form-group {
@@ -83,12 +93,13 @@ input[type="password"] {
   padding: 0.5rem;
   border: 1px solid #ccc;
   border-radius: 4px;
+  color: white;
 }
 
 button {
   width: 100%;
   padding: 0.5rem;
-  background-color: #007bff;
+  background-color: rgb(63, 63, 75);;
   color: #fff;
   border: none;
   border-radius: 4px;
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,3 +1,23 @@
 <template>
-    <h1>Dashboard</h1>
+    <div class="top">
+        <h4>Jhomer Opulencia</h4>
+        <input class="search" type="text" placeholder="Search ..."> 
+        <p>Account <span>  <v-icon style="background-color: ;" icon="mdi-account" /></span></p>
+    </div>
 </template>
+
+<style scoped>
+.top {
+    background-color: aliceblue;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 40px;
+}
+.search {
+    margin-left: 10px;
+    background-color: rgb(238, 238, 238);
+    border-radius: 40px;
+    font-size: 12px;
+}
+</style>
