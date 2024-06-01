3a93ddb11385b78d39108c393ccbeea72616bafc
JHMR18 <opulenciajhomer@gmail.com>
2024-06-01 22:47:55 +0800

    save changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Login from '../components/Login.vue'
+import LoginComponent from '../components/Login.vue'
 import Admin from '../components/Admin_Login.vue'
 import Dashboard from '../views/Dashboard.vue'
-console.log('Login:', Login); // Check if the component is being imported correctly
-console.log('Admin:', Admin);
-console.log('Dashboard:', Dashboard);
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'Login',
-      component: Login
+      name: 'login',
+      component: LoginComponent
     },
     {
-    path: '/dashboard',
-    name: 'dashboard',
-    component: Dashboard,
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
     },
     {
-    path: '/admin_login',
-    name: 'admin_login',
-    component: Admin,
+      path: '/admin_login',
+      name: 'admin_login',
+      component: Admin,
     }
   ]
 })
