cb618a0f153fc009befc0b4f3d023da7c457ef9a
Jien <jien@s-macbook-air.svg.openit.local>
2024-06-01 06:45:47 -0400

    initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../components/Login.vue'
 import Dashboard from '../views/Dashboard.vue'
+import AdminDashboard from '@/views/AdminDashboard.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -13,7 +14,10 @@ const router = createRouter({
     path: '/dashboard',
     name: 'dashboard',
     component: Dashboard,
-    }
+    },
+    {path: '/admin', 
+    name: 'admin', 
+    component: AdminDashboard}
   ]
 })
 
